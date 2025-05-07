3d58cc73af49961d9da0e488d1cca4430d379b3f
Dylanrock333 <dylanrock333@gmail.com>
2025-05-06 23:59:15 -0500

    Some stuff on the about will change soon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,10 @@ function About() {
             Your local Tech Cowboy and Professional 'Hello World' developer.
           </p>
           <p>
-            add stuff here
+          As an engineer passionate about merging technical expertise with creative innovation, I'm captivated by the transformative potential of artificial intelligence. 
+          Large Language Models can extract meaning from content, restructure information, and make agentic assessments. 
+          The agenticity of AI represents just one example of how these capabilities, when thoughtfully implemented, can revolutionize numerous fields and streamline complex tasks.
+          
           </p>
         </div>
         <div className="about-image">
